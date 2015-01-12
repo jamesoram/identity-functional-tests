fac17d7cedea64003f5ea4d55984dba6c72814d5
James Oram <herespam1@hotmail.com>
2015-01-12 16:05:26 +0000

    Add parallel test execution

--- common-test-lib/src/main/scala/com/gu/integration/test/SeleniumTestSuite.scala
+++ common-test-lib/src/main/scala/com/gu/integration/test/SeleniumTestSuite.scala
@@ -4,9 +4,9 @@ import com.gu.automation.core.WebDriverFeatureSpec
 import com.gu.automation.support.{Browser, TestRetries}
 import com.gu.integration.test.config.WebdriverInitialiser.augmentWebDriver
 import org.openqa.selenium.WebDriver
-import org.scalatest.Matchers
+import org.scalatest.{ParallelTestExecution, Matchers}
 
-abstract class SeleniumTestSuite extends WebDriverFeatureSpec with Matchers with TestRetries {
+abstract class SeleniumTestSuite extends WebDriverFeatureSpec with Matchers with TestRetries with ParallelTestExecution {
 
   override protected def startDriver(testName: String, targetBrowser: Browser): WebDriver = {
     augmentWebDriver(super.startDriver(testName, targetBrowser))
