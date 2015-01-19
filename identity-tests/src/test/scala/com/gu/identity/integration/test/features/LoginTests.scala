d28ddb28f38bd1cc0947a1323f6a4b29ed95ed24
James Oram <herespam1@hotmail.com>
2015-01-19 17:51:03 +0000

    remove social sign in tests

--- identity-tests/src/test/scala/com/gu/identity/integration/test/features/LoginTests.scala
+++ identity-tests/src/test/scala/com/gu/identity/integration/test/features/LoginTests.scala
@@ -16,20 +16,20 @@ class LoginTests extends IdentitySeleniumTestSuite {
       SignInSteps().checkUserIsLoggedInSecurely()
     }
 
-    scenarioWeb("should be able to login using existing Facebook account") { implicit driver: WebDriver =>
-      BaseSteps().goToStartPage()
-      SignInSteps().signInUsingFaceBook()
-      SignInSteps().checkUserIsLoggedIn(get("faceBookLoginName"))
-      SignInSteps().checkUserIsLoggedInSecurely()
-      SignInSteps().checkLoggedInThroughSocialMedia()
-    }
-
-    scenarioWeb("should be able to login using existing Google account") { implicit driver: WebDriver =>
-      BaseSteps().goToStartPage()
-      SignInSteps().signInUsingGoogle()
-      SignInSteps().checkUserIsLoggedIn(get("googleLoginName"))
-      SignInSteps().checkUserIsLoggedInSecurely()
-      SignInSteps().checkLoggedInThroughSocialMedia()
-    }
+//    scenarioWeb("should be able to login using existing Facebook account") { implicit driver: WebDriver =>
+//      BaseSteps().goToStartPage()
+//      SignInSteps().signInUsingFaceBook()
+//      SignInSteps().checkUserIsLoggedIn(get("faceBookLoginName"))
+//      SignInSteps().checkUserIsLoggedInSecurely()
+//      SignInSteps().checkLoggedInThroughSocialMedia()
+//    }
+//
+//    scenarioWeb("should be able to login using existing Google account") { implicit driver: WebDriver =>
+//      BaseSteps().goToStartPage()
+//      SignInSteps().signInUsingGoogle()
+//      SignInSteps().checkUserIsLoggedIn(get("googleLoginName"))
+//      SignInSteps().checkUserIsLoggedInSecurely()
+//      SignInSteps().checkLoggedInThroughSocialMedia()
+//    }
   }
 }
