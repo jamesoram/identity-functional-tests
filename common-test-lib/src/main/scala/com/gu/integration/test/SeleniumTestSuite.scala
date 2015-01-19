48346e31723ae113d504f18e0cefd260093f5d69
James Oram <herespam1@hotmail.com>
2015-01-19 17:01:54 +0000

    remove parallel test execution

--- common-test-lib/src/main/scala/com/gu/integration/test/SeleniumTestSuite.scala
+++ common-test-lib/src/main/scala/com/gu/integration/test/SeleniumTestSuite.scala
@@ -4,9 +4,9 @@ import com.gu.automation.core.WebDriverFeatureSpec
 import com.gu.automation.support.{Browser, TestRetries}
 import com.gu.integration.test.config.WebdriverInitialiser.augmentWebDriver
 import org.openqa.selenium.WebDriver
-import org.scalatest.{ParallelTestExecution, Matchers}
+import org.scalatest.Matchers
 
-abstract class SeleniumTestSuite extends WebDriverFeatureSpec with Matchers with TestRetries with ParallelTestExecution {
+abstract class SeleniumTestSuite extends WebDriverFeatureSpec with Matchers with TestRetries {
 
   override protected def startDriver(testName: String, targetBrowser: Browser): WebDriver = {
     augmentWebDriver(super.startDriver(testName, targetBrowser))
