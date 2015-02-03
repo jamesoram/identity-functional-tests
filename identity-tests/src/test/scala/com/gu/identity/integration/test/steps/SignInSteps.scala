951675154e8e3e652d9894c63fcdc72b2361ffd8
James Oram <herespam1@hotmail.com>
2015-02-03 10:26:35 +0000

    change expected name to contain a string due to teamcity limitations

--- identity-tests/src/test/scala/com/gu/identity/integration/test/steps/SignInSteps.scala
+++ identity-tests/src/test/scala/com/gu/identity/integration/test/steps/SignInSteps.scala
@@ -44,7 +44,7 @@ case class SignInSteps(implicit driver: WebDriver) extends TestLogging with Matc
   def checkUserIsLoggedIn(expectedLoginName: String) = {
     logger.step(s"Checking that user is logged in")
     val loginName = new ContainerWithSigninModulePage().signInModule().signInName.getText
-    loginName should be(expectedLoginName)
+    loginName should contain(expectedLoginName)
 
     val loginCookie = getCookie(LoginCookie)
     loginCookie.getValue should not be empty
