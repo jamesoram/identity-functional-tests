62d483a0b6bd1671f969faea324ab7f9968b2e45
James Oram <herespam1@hotmail.com>
2015-02-16 16:49:49 +0000

    remove beta redirect by default

--- common-test-lib/src/main/scala/com/gu/integration/test/steps/BaseSteps.scala
+++ common-test-lib/src/main/scala/com/gu/integration/test/steps/BaseSteps.scala
@@ -1,20 +1,19 @@
 package com.gu.integration.test.steps
 
 import com.gu.automation.support.TestLogging
-import com.gu.integration.test.pages.common.{TermsOfServicePage, ParentPage}
-import com.gu.integration.test.util.PageLoader
+import com.gu.integration.test.pages.common.{ParentPage, TermsOfServicePage}
 import com.gu.integration.test.util.PageLoader._
 import org.openqa.selenium.WebDriver
 import org.scalatest.Matchers
 
 case class BaseSteps(implicit driver: WebDriver) extends TestLogging with Matchers {
-  def goToStartPage(useBetaRedirect: Boolean = true): ParentPage = {
+  def goToStartPage(useBetaRedirect: Boolean = false): ParentPage = {
     logger.step(s"I am on base page at url: $frontsBaseUrl")
     lazy val parentPage = new ParentPage()
     goTo(parentPage, frontsBaseUrl, useBetaRedirect)
   }
 
-  def goToTermsOfServicePage(useBetaRedirect: Boolean = true): TermsOfServicePage = {
+  def goToTermsOfServicePage(useBetaRedirect: Boolean = false): TermsOfServicePage = {
     logger.step(s"I am on base page at url: $frontsBaseUrl")
     lazy val tosPage = new TermsOfServicePage()
     goTo(tosPage, fromRelativeUrl("/help/terms-of-service"))
