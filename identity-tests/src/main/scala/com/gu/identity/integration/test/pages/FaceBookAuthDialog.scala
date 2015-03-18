2f383d623735a72380e16e6b0203833a988fe552
Niklas Lindblad <niklas.lindblad@theguardian.co.uk>
2015-03-18 16:19:07 +0000

    Wait until elements are visible before returning their reference

--- identity-tests/src/main/scala/com/gu/identity/integration/test/pages/FaceBookAuthDialog.scala
+++ identity-tests/src/main/scala/com/gu/identity/integration/test/pages/FaceBookAuthDialog.scala
@@ -1,8 +1,28 @@
 package com.gu.identity.integration.test.pages
 
 import com.gu.integration.test.pages.common.ParentPage
+import org.openqa.selenium.support.ui.{WebDriverWait, ExpectedConditions}
 import org.openqa.selenium.{By, WebDriver, WebElement}
 
 class FaceBookAuthDialog(implicit driver: WebDriver) extends ParentPage {
-  def confirmButton: WebElement = driver.findElement(By.xpath("//button[@name='__CONFIRM__']"))
+  def confirmButton: WebElement = {
+    val xpath = "//button[@name='__CONFIRM__']"
+    val wait = new WebDriverWait(driver, 10);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)))
+    driver.findElement(By.xpath(xpath))
+  }
+
+  def editInformationProvided: WebElement = {
+    val xpath = "//a[text()='Edit the info you provide']"
+    val wait = new WebDriverWait(driver, 10);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)))
+    driver.findElement(By.xpath(xpath))
+  }
+
+  def emailCheckBox: WebElement = {
+    val xpath = "(//span[preceding::input[@value='email']])[1]"
+    val wait = new WebDriverWait(driver, 10);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)))
+    driver.findElement(By.xpath(xpath))
+  }
 }
