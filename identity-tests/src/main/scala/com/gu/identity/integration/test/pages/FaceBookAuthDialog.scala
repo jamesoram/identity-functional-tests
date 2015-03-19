b968d9c12a146bdc79f893cd2ad3dbe0d365a48b
Niklas Lindblad <niklas.lindblad@theguardian.co.uk>
2015-03-19 11:05:33 +0000

    Group private methods and add better selectors for Facebook auth dialog

--- identity-tests/src/main/scala/com/gu/identity/integration/test/pages/FaceBookAuthDialog.scala
+++ identity-tests/src/main/scala/com/gu/identity/integration/test/pages/FaceBookAuthDialog.scala
@@ -6,7 +6,21 @@ import org.openqa.selenium.{By, WebDriver, WebElement}
 
 class FaceBookAuthDialog(implicit driver: WebDriver) extends ParentPage {
   private def confirmButton: WebElement = {
-    val xpath = "//button[@name='__CONFIRM__']"
+    val xpath = "id('platformDialogForm')//button[@name='__CONFIRM__']"
+    val wait = new WebDriverWait(driver, 10);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)))
+    driver.findElement(By.xpath(xpath))
+  }
+
+  private def editInformationProvided: WebElement = {
+    val id = "u_0_m"
+    val wait = new WebDriverWait(driver, 10);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(id)))
+    driver.findElement(By.id(id))
+  }
+
+  private def emailCheckBox: WebElement = {
+    val xpath = "(//span[preceding::input[@value='email']])[1]"
     val wait = new WebDriverWait(driver, 10);
     wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)))
     driver.findElement(By.xpath(xpath))
@@ -16,23 +30,9 @@ class FaceBookAuthDialog(implicit driver: WebDriver) extends ParentPage {
     confirmButton.click()
   }
 
-  private def editInformationProvided: WebElement = {
-    val xpath = "//a[text()='Edit the info you provide']"
-    val wait = new WebDriverWait(driver, 10);
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)))
-    driver.findElement(By.xpath(xpath))
-  }
-
   def clickEditInformationProvided() = {
     editInformationProvided.click()
     this
-  }
-
-  private def emailCheckBox: WebElement = {
-    val xpath = "(//span[preceding::input[@value='email']])[1]"
-    val wait = new WebDriverWait(driver, 10);
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)))
-    driver.findElement(By.xpath(xpath))
   }
 
   def clickEmailCheckBox() = {
