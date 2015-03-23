a97bef5a0f40e2e5b32e1474193c442d9353fd77
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2015-03-23 11:29:24 +0000

    Use CSS selector instead of XPath and remove explicit wait

--- identity-tests/src/main/scala/com/gu/identity/integration/test/pages/PasswordResetSentPage.scala
+++ identity-tests/src/main/scala/com/gu/identity/integration/test/pages/PasswordResetSentPage.scala
@@ -5,10 +5,7 @@ import org.openqa.selenium.{By, WebDriver, WebElement}
 
 class PasswordResetSentPage(implicit driver: WebDriver) extends ContainerWithSigninModulePage {
   private def message: WebElement = {
-    val xpath = "//h1[@class='identity-title']"
-    val wait = new WebDriverWait(driver, 10);
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)))
-    driver.findElement(By.xpath(xpath))
+    driver.findElement(By.cssSelector(".identity-title"))
   }
 
  def getMessagText(): String = {
