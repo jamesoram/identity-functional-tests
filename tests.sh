b5218d5f78942aec148c613a5d0cf3db9e58af14
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2015-03-26 12:18:59 +0000

    Rename configuration files to code and dev. Download msising conf from S3.

--- tests.sh
+++ tests.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-CONF_FILE="local.conf"
-if [[ $1 = "code" ]]; then
-    CONF_FILE="dev.conf"
+
+which aws 1>/dev/null || exit 1
+
+BUCKET="identity-functional-tests"
+
+LOCAL_CONF_FILE="dev.conf"
+REMOTE_CONF_FILE="s3://${BUCKET}/DEV/dev.conf"
+if [ "$1" = "code" ]; then
+    LOCAL_CONF_FILE="code.conf"
+    REMOTE_CONF_FILE="s3://${BUCKET}/CODE/code.conf"
 fi
 
-echo "${CONF_FILE}"
-sbt -Dconfig.resource=${CONF_FILE} "project identity-tests" "test"
+if [ ! -f "./identity-tests/${LOCAL_CONF_FILE}" ]; then
+    aws s3 cp "${REMOTE_CONF_FILE}" "./identity-tests/${LOCAL_CONF_FILE}"
+fi
+
+echo "Running tests using config file: ${LOCAL_CONF_FILE}"
+sbt -Dconfig.resource=${LOCAL_CONF_FILE} "project identity-tests" "test"
