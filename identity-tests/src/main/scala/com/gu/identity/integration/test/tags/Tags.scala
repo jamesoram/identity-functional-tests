078550320e1cc39292e6377a9231c9d7c3103e08
Sean Pike <pike.sean@gmail.com>
2015-06-11 11:00:34 +0100

    updates tags to tag core and optional tests

--- identity-tests/src/main/scala/com/gu/identity/integration/test/tags/Tags.scala
+++ identity-tests/src/main/scala/com/gu/identity/integration/test/tags/Tags.scala
@@ -4,17 +4,22 @@ import org.scalatest.Tag
 
   /* Usage: The tag is added as a comma separated list for each scenario after the scenario name
   e.g. scenarioWeb("Example test that is unstable", Small, Unstable)
-    Running Tests:  The Run/Debug configuration can specify which tags you want to run or ignore using the tag text
-    e.g. to run only small tests but ignore unstable or large tests you would use the following in Test Options
-    -n small -l large -l unstable
+    Running tests locally:  The Run/Debug configuration can specify which tags you want to run or ignore using the tag
+    text e.g. to run only core tests but ignore unstable or optional tests you would use the following in Test Options
+    -n CoreTest -l OptionalTest -l Unstable
+    Running tests TeamCity: In Edit Configuration Settings -> Build Step -> SBT Commands enter the following
+    "project identity-tests" clean compile "test-only -- -l unstable"
+    to add further tags use this format "test-only -- -l Unstable -l OptionalTest"
+    NOTE: Ideally you should not use -n as this will ignore tests that are untagged. By only using -l these untagged
+    tests will also be run
   */
 
-  //tests intermittently fail, e.g. tests running against 3rd party sites are more reliant on stable connections
-  object Unstable extends Tag("unstable")
+  //test intermittently fails, e.g. tests running against 3rd party sites are more reliant on stable connections
+  object Unstable extends Tag("Unstable")
 
-  //test usually finishes within 60 seconds
-  object Small extends Tag("small")
+  //test is essential and the site will not function without this feature
+  object CoreTest extends Tag("CoreTest")
 
-  //test usually finishes after 60 seconds
-  object Large extends Tag("large")
+  //test is important but the site can handle this feature missing for a short time
+  object OptionalTest extends Tag("OptionalTest")
 
--- identity-tests/src/test/scala/com/gu/identity/integration/test/features/LoginTests.scala
+++ identity-tests/src/test/scala/com/gu/identity/integration/test/features/LoginTests.scala
@@ -3,7 +3,7 @@ package com.gu.identity.integration.test.features
 import com.gu.automation.support.Config
 import com.gu.identity.integration.test.IdentitySeleniumTestSuite
 import com.gu.identity.integration.test.steps.SignInSteps
-import com.gu.identity.integration.test.tags.{Large, Small}
+import com.gu.identity.integration.test.tags.{OptionalTest, CoreTest}
 import com.gu.integration.test.steps.BaseSteps
 import com.gu.integration.test.util.UserConfig._
 import org.openqa.selenium.WebDriver
@@ -11,21 +11,21 @@ import org.openqa.selenium.WebDriver
 class LoginTests extends IdentitySeleniumTestSuite {
 
   feature("Login feature") {
-    scenarioWeb("should be able to login using credentials", Small) { implicit driver: WebDriver =>
+    scenarioWeb("should be able to login using credentials", CoreTest) { implicit driver: WebDriver =>
       BaseSteps().goToStartPage()
       SignInSteps().signIn()
       SignInSteps().checkUserIsLoggedIn(get("loginName"))
       SignInSteps().checkUserIsLoggedInSecurely()
     }
 
-    scenarioWeb("should be able to login using existing Facebook account", Large) { implicit driver: WebDriver =>
+    scenarioWeb("should be able to login using existing Facebook account", OptionalTest) { implicit driver: WebDriver =>
       BaseSteps().goToStartPage()
       SignInSteps().signInUsingFaceBook()
       SignInSteps().checkUserIsLoggedIn(Config().getUserValue("faceBookLoginName"))
       SignInSteps().checkUserIsLoggedInSecurely()
     }
 
-    scenarioWeb("should be able to login using existing Google account", Large) { implicit driver: WebDriver =>
+    scenarioWeb("should be able to login using existing Google account", OptionalTest) { implicit driver: WebDriver =>
       BaseSteps().goToStartPage()
       SignInSteps().signInUsingGoogle()
       SignInSteps().checkUserIsLoggedIn(Config().getUserValue("googleLoginName"))
