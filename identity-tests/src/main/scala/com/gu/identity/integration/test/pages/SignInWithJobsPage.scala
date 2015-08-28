945c660d3fededf76586f6f9e4c946e223bcd465
markjamesbutler <markjamesbutler@gmail.com>
2015-08-28 20:36:24 +0100

    Fixed lookup of privacy and t&c's links.

--- identity-tests/src/main/scala/com/gu/identity/integration/test/pages/SignInWithJobsPage.scala
+++ identity-tests/src/main/scala/com/gu/identity/integration/test/pages/SignInWithJobsPage.scala
@@ -1,11 +1,15 @@
 package com.gu.identity.integration.test.pages
 
-import org.openqa.selenium.WebDriver
+import com.gu.integration.test.util.ElementLoader._
+import org.openqa.selenium.{WebElement, WebDriver}
 
 class SignInWithJobsPage(implicit driver: WebDriver) extends SignInPage {
 
+  private def jobsTC: WebElement = findByTestAttribute("jobs-tc")
+  private def jobsPrivacy: WebElement = findByTestAttribute("jobs-privacy")
+
   def checkTermsVisible: Boolean = {
-    driver.getPageSource.contains("Guardian Jobs")
+    jobsTC.isDisplayed && jobsPrivacy.isDisplayed
   }
 
 }
