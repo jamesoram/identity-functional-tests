d99d2fec5ea718c20e71516d1c1d3c86c73f27bc
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2015-09-02 17:18:12 +0100

    Use jobs stub URL from config

--- identity-tests/src/test/scala/com/gu/identity/integration/test/steps/TermsAndConditionsSteps.scala
+++ identity-tests/src/test/scala/com/gu/identity/integration/test/steps/TermsAndConditionsSteps.scala
@@ -1,13 +1,13 @@
 package com.gu.identity.integration.test.steps
 
-import com.gu.automation.support.TestLogging
+import com.gu.automation.support.{Config, TestLogging}
 import com.gu.identity.integration.test.pages.{ThirdPartyConditions, JobsStubPage}
 import org.openqa.selenium.WebDriver
 import org.scalatest.Matchers
 
 case class TermsAndConditionsSteps(implicit driver: WebDriver) extends TestLogging with Matchers {
 
-  val jobsStub = "http://public-jobs-thegulocal-com.s3-website-eu-west-1.amazonaws.com"
+  val jobsStub = Config().getUserValue("jobsStubUrl")
 
   def goToJobsSite: JobsStubPage = {
     logger.step(s"I am on stub jobs page at url: $jobsStub")
