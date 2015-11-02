24f4ef6ca3584bc9c25844fad9856899c34c271a
markjamesbutler <markjamesbutler@gmail.com>
2015-11-02 15:45:05 +0000

    Add smoke tests tag.

--- identity-tests/src/test/scala/com/gu/identity/integration/test/features/LoginTests.scala
+++ identity-tests/src/test/scala/com/gu/identity/integration/test/features/LoginTests.scala
@@ -3,7 +3,7 @@ package com.gu.identity.integration.test.features
 import com.gu.automation.support.Config
 import com.gu.identity.integration.test.IdentitySeleniumTestSuite
 import com.gu.identity.integration.test.steps.SignInSteps
-import com.gu.identity.integration.test.tags.{SocialTest, OptionalTest, CoreTest}
+import com.gu.identity.integration.test.tags.{SmokeTest, SocialTest, OptionalTest, CoreTest}
 import com.gu.integration.test.steps.BaseSteps
 import com.gu.integration.test.util.UserConfig._
 import org.openqa.selenium.WebDriver
@@ -11,7 +11,7 @@ import org.openqa.selenium.WebDriver
 class LoginTests extends IdentitySeleniumTestSuite {
 
   feature("Login feature") {
-    scenarioWeb("L1: should be able to login using credentials", CoreTest) { implicit driver: WebDriver =>
+    scenarioWeb("L1: should be able to login using credentials", CoreTest, SmokeTest) { implicit driver: WebDriver =>
       BaseSteps().goToStartPage()
       SignInSteps().signIn()
       SignInSteps().checkUserIsLoggedIn(get("loginName"))
